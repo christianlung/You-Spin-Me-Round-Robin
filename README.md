ff274e61022bfb6bf97a296a86bb40494c0df500
Christian Lung <christianlung@Christians-MacBook-Air.local>
2023-05-09 01:33:42 -0700

    Complete round robin scheduling policy

---
+++ README.md
@@ -0,0 +1,21 @@
+## UID: 005731615
+(IMPORTANT: Only replace the above numbers with your true UID, do not modify spacing and newlines, otherwise your tarfile might not be created correctly)
+
+## You Spin Me Round Robin
+Simulation for round robin scheduling given a workload and quantum length
+
+## Building
+
+Explain briefly how to build your program
+Run the command "make"
+
+## Running
+
+Show an example run of your program, using at least two additional arguments, and what to expect
+./rr processes.txt 3
+You should expect two lines: 1 line for average wait time and 1 line for average response time
+
+## Cleaning up
+
+Explain briefly how to clean up all binary files
+Run the command "make clean"
--- rr.c
+++ rr.c
@@ -21,6 +21,9 @@ struct process
   TAILQ_ENTRY(process) pointers;
 
   /* Additional fields here */
+  u32 remaining_time;
+  u32 end_time;
+  u32 start_execute_time;
   /* End of "Additional fields here" */
 };
 
@@ -141,6 +144,14 @@ void init_processes(const char *path,
   close(fd);
 }
 
+void visual(struct process_list list){
+  struct process *np;
+  TAILQ_FOREACH(np, &list, pointers){
+    printf("%d\t", np->pid);
+  }
+  printf("\n");
+}
+
 int main(int argc, char *argv[])
 {
   if (argc != 3)
@@ -158,8 +169,74 @@ int main(int argc, char *argv[])
 
   u32 total_waiting_time = 0;
   u32 total_response_time = 0;
-
+  
   /* Your code here */
+  u32 universal_time = 0;
+
+  //initialized all remaining times
+  for(int proc=0; proc<size; proc++){
+    data[proc].remaining_time = data[proc].burst_time;
+  }
+
+  //add new processes at time 0
+  for(int proc=0; proc<size; proc++){
+    if(data[proc].arrival_time==0){
+      // printf("Add initial P%d: \t%d\n", data[proc].pid, data[proc].remaining_time);
+      //add to queue
+      TAILQ_INSERT_TAIL(&list, &data[proc], pointers);
+    }
+  }
+  // visual(list);
+
+  bool finished = false;
+  while(!finished){
+    //pop 1st process from front
+    struct process *first = TAILQ_FIRST(&list);
+    if(first == NULL){
+      continue;
+    }
+    // printf("Remove P%d: \t%d\n", first->pid, first->remaining_time);
+    TAILQ_REMOVE(&list, first, pointers);
+    // visual(list);
+    //run process for min(quantum, remaining time)
+    int min_time = (quantum_length < first->remaining_time) ? quantum_length : first->remaining_time;
+    for(int t=0; t<min_time; t++){
+      if(first->remaining_time == first->burst_time) first->start_execute_time = universal_time;
+      first->remaining_time--;
+      universal_time++;
+      //add any new processes just arrived
+      for(int proc=0; proc<size; proc++){
+        if(data[proc].arrival_time==universal_time){
+          // printf("Add P%d: \t%d\n", data[proc].pid, data[proc].remaining_time);
+          // visual(list);
+          TAILQ_INSERT_TAIL(&list, &data[proc], pointers);
+        }
+      }
+    }
+    //if remaining time > 0 add back in queue
+    if(first->remaining_time>0){
+      // printf("ReAdd P%d: \t%d\n", first->pid, first->remaining_time);
+      TAILQ_INSERT_TAIL(&list, first, pointers);
+      // visual(list);
+    }
+    else{
+      first->end_time = universal_time;
+      finished = true;
+      struct process *np;
+      TAILQ_FOREACH(np, &list, pointers){
+        if(np->remaining_time>0){
+          finished = false;
+          break;
+        }
+      }
+    }
+  }
+  //calculate times
+  for(int proc=0; proc<size; proc++){
+    total_waiting_time += data[proc].end_time - data[proc].arrival_time - data[proc].burst_time;
+    total_response_time += data[proc].start_execute_time - data[proc].arrival_time;
+  }
+  
   /* End of "Your code here" */
 
   printf("Average waiting time: %.2f\n", (float)total_waiting_time / (float)size);
